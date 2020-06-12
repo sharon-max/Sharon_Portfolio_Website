748ca827bd8e36b35f620e1972a65913660fab9a
iamsharonjp <sharon@adadev.org.za>
2020-06-12 15:41:09 +0200

    done home page on mobile

--- js/myJs.js
+++ js/myJs.js
@@ -1,15 +1,23 @@
-const hide = document.querySelector(".hide");
-const menu = document.querySelector(".menu-icon");
-const close = document.querySelector(".close");
+// const hide = document.querySelector(".hide");
+// const menu = document.querySelector(".menu-icon");
+// const close = document.querySelector(".close");
 
-hide.style.display="none";
+// hide.style.display="none";
 
 
 
-menu.addEventListener( "click", () => {
-    hide.style.display="block";
-});
+// menu.addEventListener( "click", () => {
+//     hide.style.display="block";
+// });
 
-close.addEventListener("click",() =>{
-    hide.style.display="none";
-});
+// close.addEventListener("click",() =>{
+//     hide.style.display="none";
+// });
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
